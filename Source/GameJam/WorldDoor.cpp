fcc7481e3161b493ddb3b209154cce2bfe05f745
johdav999 <johandavidsson@hotmail.se>
2025-10-07 22:19:15 +0200

    Refactor world door to use shift behavior component

--- Source/GameJam/WorldDoor.cpp
+++ Source/GameJam/WorldDoor.cpp
@@ -1,7 +1,8 @@
 #include "WorldDoor.h"
 
+#include "ShiftPlatform.h"
 #include "WorldManager.h"
-#include "WorldShiftComponent.h"
+#include "WorldShiftBehaviorComponent.h"
 #include "Components/StaticMeshComponent.h"
 #include "Kismet/GameplayStatics.h"
 #include "Sound/SoundBase.h"
@@ -13,68 +14,80 @@ AWorldDoor::AWorldDoor()
     DoorMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("DoorMesh"));
     RootComponent = DoorMesh;
 
-    WorldShiftComponent = CreateDefaultSubobject<UWorldShiftComponent>(TEXT("WorldShiftComponent"));
+    WorldShiftBehavior = CreateDefaultSubobject<UWorldShiftBehaviorComponent>(TEXT("WorldShiftBehavior"));
+    if (WorldShiftBehavior)
+    {
+        WorldShiftBehavior->SetTargetMesh(DoorMesh);
+    }
 
     DoorMesh->SetCollisionProfileName(TEXT("BlockAll"));
     DoorMesh->SetGenerateOverlapEvents(false);
 
     bAnimateOnToggle = true;
-    bIsCurrentlyVisible = true;
+    bIsCurrentlySolid = true;
     bHasInitialized = false;
+
+    SolidInWorlds = {EWorldState::Light};
 }
 
 void AWorldDoor::BeginPlay()
 {
     Super::BeginPlay();
 
-    if (WorldShiftComponent)
-    {
-        WorldShiftComponent->OnWorldShifted.AddDynamic(this, &AWorldDoor::HandleWorldShift);
-    }
+    InitializeWorldBehaviors();
 
     if (AWorldManager* Manager = AWorldManager::Get(GetWorld()))
     {
+        CachedWorldManager = Manager;
+        Manager->OnWorldShifted.AddDynamic(this, &AWorldDoor::HandleWorldShift);
         HandleWorldShift(Manager->GetCurrentWorld());
     }
     else
     {
-        const bool bShouldBeVisible = VisibleInWorlds.Contains(EWorldState::Light);
-        SetDoorState(bShouldBeVisible);
+        SetDoorState(IsSolidInWorld(EWorldState::Light));
     }
+}
+
+void AWorldDoor::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+    if (CachedWorldManager.IsValid())
+    {
+        if (AWorldManager* Manager = CachedWorldManager.Get())
+        {
+            Manager->OnWorldShifted.RemoveDynamic(this, &AWorldDoor::HandleWorldShift);
+        }
+
+        CachedWorldManager.Reset();
+    }
+
+    Super::EndPlay(EndPlayReason);
 }
 
 void AWorldDoor::HandleWorldShift(EWorldState NewWorld)
 {
-    const bool bShouldBeVisible = VisibleInWorlds.Contains(NewWorld);
-    SetDoorState(bShouldBeVisible);
+    SetDoorState(IsSolidInWorld(NewWorld));
 }
 
-void AWorldDoor::SetDoorState(bool bShouldBeVisible)
+void AWorldDoor::SetDoorState(bool bShouldBeSolid)
 {
-    if (bHasInitialized && bIsCurrentlyVisible == bShouldBeVisible)
+    if (bHasInitialized && bIsCurrentlySolid == bShouldBeSolid)
     {
         return;
     }
 
     bHasInitialized = true;
-    bIsCurrentlyVisible = bShouldBeVisible;
-
-    if (DoorMesh)
-    {
-        DoorMesh->SetHiddenInGame(!bShouldBeVisible);
-        DoorMesh->SetCollisionEnabled(bShouldBeVisible ? ECollisionEnabled::QueryAndPhysics : ECollisionEnabled::NoCollision);
-    }
+    bIsCurrentlySolid = bShouldBeSolid;
 
     if (bAnimateOnToggle)
     {
-        PlayDoorAnimation(bShouldBeVisible);
+        PlayDoorAnimation(!bShouldBeSolid);
     }
 
-    if (bShouldBeVisible && OpenSound)
+    if (!bShouldBeSolid && OpenSound)
     {
         UGameplayStatics::PlaySoundAtLocation(this, OpenSound, GetActorLocation());
     }
-    else if (!bShouldBeVisible && CloseSound)
+    else if (bShouldBeSolid && CloseSound)
     {
         UGameplayStatics::PlaySoundAtLocation(this, CloseSound, GetActorLocation());
     }
@@ -89,4 +102,39 @@ void AWorldDoor::PlayDoorAnimation(bool bOpening)
 
     const FRotator TargetRotation = bOpening ? FRotator(0.f, 90.f, 0.f) : FRotator::ZeroRotator;
     DoorMesh->SetRelativeRotation(TargetRotation);
+}
+
+bool AWorldDoor::IsSolidInWorld(EWorldState World) const
+{
+    if (WorldShiftBehavior)
+    {
+        if (const EPlatformState* FoundState = WorldShiftBehavior->WorldBehaviors.Find(World))
+        {
+            return *FoundState == EPlatformState::Solid || *FoundState == EPlatformState::TimedSolid;
+        }
+    }
+
+    return SolidInWorlds.Contains(World);
+}
+
+void AWorldDoor::InitializeWorldBehaviors()
+{
+    if (!WorldShiftBehavior)
+    {
+        return;
+    }
+
+    WorldShiftBehavior->SetTargetMesh(DoorMesh);
+
+    if (WorldShiftBehavior->WorldBehaviors.Num() > 0)
+    {
+        return;
+    }
+
+    const TArray<EWorldState> AllWorlds = {EWorldState::Light, EWorldState::Shadow, EWorldState::Chaos};
+    for (EWorldState World : AllWorlds)
+    {
+        const bool bSolid = SolidInWorlds.Contains(World);
+        WorldShiftBehavior->WorldBehaviors.Add(World, bSolid ? EPlatformState::Solid : EPlatformState::Ghost);
+    }
 }
